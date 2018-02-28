5bd6accaa72593e11525851e9d1ca1cfe11a4957
Jason Lenoble <jason.lenoble@gmail.com>
2018-02-28 13:40:03 +0100

    Timeblock extends Twix
    It overrides split and divide.

--- src/timeblock.js
+++ src/timeblock.js
@@ -1,5 +1,64 @@
-export default class Timeblock {
-  constructor () {
-    console.log('Hello world!');
+import Twix from 'twix';
+
+export default class Timeblock extends Twix {
+  constructor (a, b) {
+    if (a instanceof Twix) {
+      super(a._start, a._end);
+    } else {
+      super(a, b);
+    }
+
+    let _children = [];
+    let _parent = null;
+
+    Object.defineProperties(this, {
+      _parent: {
+        get () {
+          return _parent;
+        },
+        set (tb) {
+          if (!_parent) {
+            if (tb instanceof Timeblock) {
+              _parent = tb;
+            } else {
+              throw new Error('Bad assignment type for timeblock parent');
+            }
+          } else {
+            throw new Error('Parent of timeblock already set');
+          }
+        },
+      },
+
+      _children: {
+        get () {
+          return _children;
+        },
+        set (array) {
+          _children = array.map(tw => {
+            const tb = new Timeblock(tw);
+            tb._parent = this;
+            return tb;
+          });
+        },
+      },
+
+      _root: {
+        get () {
+          return _parent ? _parent._root : this;
+        },
+      },
+    });
+  }
+
+  split (...args) {
+    const twixes = super.split(...args);
+    this._children = twixes;
+    return twixes;
+  }
+
+  divide (n) {
+    const twixes = super.divide(n);
+    this._children = twixes;
+    return twixes;
   }
 }
--- test/timeblock.test.js
+++ test/timeblock.test.js
@@ -1,12 +1,72 @@
-import Muter, {captured} from 'muter';
 import {expect} from 'chai';
+import moment from 'moment';
+import Twix from 'twix';
 import Timeblock from '../src/timeblock';
 
 describe('Testing Timeblock', function () {
-  const muter = Muter(console, 'log'); // eslint-disable-line new-cap
+  const today = moment().startOf('d');
+  const tomorrow = today.clone().add(1, 'd');
+  const todaySpan = moment.twix(today, tomorrow);
+  const dayFormat = todaySpan.format();
 
-  it(`Class Timeblock says 'Hello world!'`, captured(muter, function () {
-    new Timeblock();
-    expect(muter.getLogs()).to.equal('Hello world!\n');
-  }));
+  const hourSpans = [];
+  for (let i = 0; i < 24; ++i) {
+    hourSpans.push(moment.twix(today.clone().add(i, 'h'),
+      today.clone().add(i+1, 'h')));
+  }
+
+  it(`format() yields ${dayFormat}`, function () {
+    const tb1 = new Timeblock(today, today.clone().add(1, 'd'));
+    expect(tb1.format()).to.equal(dayFormat);
+
+    const tb2 = new Timeblock(todaySpan);
+    expect(tb2.format()).to.equal(dayFormat);
+
+    const tb3 = new Timeblock(tb1);
+    expect(tb3.format()).to.equal(dayFormat);
+  });
+
+  it('A Timeblock can be subdivided in equal parts', function () {
+    const tb = new Timeblock(todaySpan);
+    const blocks = tb.divide(24);
+
+    expect(blocks.length).to.equal(24);
+    blocks.forEach((blk, i) => {
+      expect(blk).not.to.be.instanceof(Timeblock);
+      expect(blk).to.be.instanceof(Twix);
+      expect(blk.format()).to.equal(hourSpans[i].format());
+    });
+
+    expect(tb._children.length).to.equal(24);
+    tb._children.forEach((child, i) => {
+      expect(child).to.be.instanceof(Timeblock);
+      expect(child.format()).to.equal(hourSpans[i].format());
+    });
+  });
+
+  it('A Timeblock can be subdivided arbitrarily', function () {
+    const tb = new Timeblock(todaySpan);
+    const t1 = today.clone().add(3, 'h');
+    const t2 = today.clone().add({h: 7, m: 25});
+    const blocks0 = [
+      moment.twix(today, t1),
+      moment.twix(t1, t2),
+      moment.twix(t2, tomorrow),
+    ];
+
+    const blocks = tb.split(t1, t2);
+
+    expect(blocks.length).to.equal(3);
+    blocks.forEach((blk, i) => {
+      expect(blk).not.to.be.instanceof(Timeblock);
+      expect(blk).to.be.instanceof(Twix);
+      expect(blk.format()).to.equal(blocks0[i].format());
+    });
+
+    expect(tb._children.length).to.equal(3);
+    tb._children.forEach((child, i) => {
+      expect(child).to.be.instanceof(Timeblock);
+      expect(child.format()).to.equal(blocks0[i].format());
+    });
+  });
 });
