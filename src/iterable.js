183fab3d13581eb57af882272304d785da2cb254
Jason Lenoble <jason.lenoble@gmail.com>
2018-03-01 17:10:17 +0100

    Added makeIterable factory
    Refactory Day and Month with it
    Added Hour, Week and Year

--- src/iterable.js
+++ src/iterable.js
@@ -1,24 +1,35 @@
 import moment from 'moment';
 import Timeblock from './timeblock';
 
-export class Day extends Timeblock {
-  constructor (start) {
-    const mt = moment(start).startOf('d');
-    super(mt, mt.clone().add(1, 'd'));
+const names = {
+  h: 'Hour',
+  d: 'Day',
+  w: 'Week',
+  M: 'Month',
+  y: 'Year',
+};
+
+const makeIterable = tag => {
+  class Iterable extends Timeblock {
+    constructor (start) {
+      const mt = moment(start).startOf(tag);
+      super(mt, mt.clone().add(1, tag));
+    }
+
+    next () {
+      return new Iterable(this.start().add(1, tag));
+    }
   }
 
-  next () {
-    return new Day(this.start().add(1, 'd'));
-  }
-}
+  Object.defineProperty(Iterable, 'name', {
+    value: names[tag],
+  });
 
-export class Month extends Timeblock {
-  constructor (start) {
-    const mt = moment(start).startOf('M');
-    super(mt, mt.clone().add(1, 'M'));
-  }
+  return Iterable;
+};
 
-  next () {
-    return new Month(this.start().add(1, 'M'));
-  }
-}
+export const Hour = makeIterable('h');
+export const Day = makeIterable('d');
+export const Week = makeIterable('w');
+export const Month = makeIterable('M');
+export const Year = makeIterable('y');
