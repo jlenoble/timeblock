88aad31bd17b46ced56de1c66fc7aa1b2eff8de9
Jason Lenoble <jason.lenoble@gmail.com>
2018-03-03 02:43:28 +0100

    Expose adaptors

--- src/compact-element.js
+++ src/compact-element.js
@@ -3,9 +3,13 @@ import {elements} from './element';
 export const makeCompactElement = Element => {
   const {adapt, clone, shift, diff} = Element.adaptors;
 
-  return class CompactElement extends Element {
+  class CompactElement extends Element {
     constructor (...args) {
       super(args[0], args[args.length - 1]);
     }
-  };
+  }
+
+  CompactElement.adaptors = Element.adaptors;
+
+  return CompactElement;
 };
